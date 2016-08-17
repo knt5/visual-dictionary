802a09b3a38b28677b1911b6027f26e21862c4a5
knt5 <knt5@users.noreply.github.com>
2016-08-17 23:37:01 +0900

    Add minimum search code

--- src/js/search/init.js
+++ src/js/search/init.js
@@ -1,1 +1,10 @@
 console.log('hi');
+
+// $('#keyword').on('keydown');
+
+const keyword = 'cat';
+const q = encodeURIComponent(keyword);
+
+$('#text-frame').prop('src', 'http://eow.alc.co.jp/sp/search.html?q=' + q);
+$('#image-frame').prop('src', 'https://www.google.co.jp/search?q=' + q + '&tbm=isch&safe=high');
+
